7a7f0ccce191be4ce4ffbc400232952a917b3aa2
Catherine Lee <43484672+itscatherinelee@users.noreply.github.com>
2023-08-29 18:29:43 -0400

    Update .sendit.sh

--- .sendit.sh
+++ .sendit.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
 # Script that helps configure a brand new setup
+echo "Configuring brand new setup"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo ".cfg" >> .gitignore
+git clone --bare https://github.com/itscatherinelee/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 # Download homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Download iterm2
 brew install --cask iterm2
+
+#Download nvm
+brew install nvm
+
+#Download vim
 brew install vim
+
+#Download tmux
 brew install tmux
 
 # Download powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # Download vim plugin
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
